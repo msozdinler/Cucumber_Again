01e6d84d6a0ef244684097646519dc56ae18a57a
msozdinler <sozdinlermertc@gmail.com>
2023-03-24 18:40:51 -0400

    Initial commit

---
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -0,0 +1,26 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class _01_LoginSteps {
+    @Given("Navigate to Campus")
+    public void navigate_to_campus() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Enter username and password")
+    public void enter_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Click on Login Button")
+    public void click_on_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User should login successfully")
+    public void user_should_login_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
